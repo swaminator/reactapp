dbcd9b3f236ca55e9e1e08ec2932f3659a11126d
Natoniewski <anatonie@dca9047db951.ant.amazon.com>
2018-08-17 16:28:27 -0700

    demo commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to the Demo</h1>
           <h1 className="App-title">This is a new change</h1>
         </header>
         <marquee>New App</marquee>
