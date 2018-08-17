f7bc6fae1252f07d01ed418872c1ff31393e82e0
anatonie <34927109+anatonie@users.noreply.github.com>
2018-08-17 11:08:40 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
           <h1 className="App-title">This is a new change</h1>
         </header>
-        <marquee>Testing StartAt feature</marquee>
+        <marquee>Testing StartAt feature #2</marquee>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
