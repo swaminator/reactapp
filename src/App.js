82154b877c55a32be5db6dd1b4fdf21c20dd8d17
anatonie <34927109+anatonie@users.noreply.github.com>
2018-08-17 13:37:39 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App5">
+      <div className="App6">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
