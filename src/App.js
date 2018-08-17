90cf5a5a915eb64db148c4c4f431bda80f4b4bb1
anatonie <34927109+anatonie@users.noreply.github.com>
2018-08-17 10:55:09 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
           <h1 className="App-title">This is a new change</h1>
         </header>
-        <marquee>Demo for Nikhil</marquee>
+        <marquee>Live Log Test</marquee>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
