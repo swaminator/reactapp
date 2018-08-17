fe181cc892465e5a9232b04f790e63cfd85ea786
anatonie <34927109+anatonie@users.noreply.github.com>
2018-08-17 11:00:43 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
           <h1 className="App-title">This is a new change</h1>
         </header>
-        <marquee>Local Server Crashed. Real Live Log Test This Time</marquee>
+        <marquee>It Works!</marquee>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
