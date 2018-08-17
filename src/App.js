c1bf4ed7066d972279bbbf4a2e1f33c74a4b792d
anatonie <34927109+anatonie@users.noreply.github.com>
2018-08-17 15:52:19 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <marquee><header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
           <h1 className="App-title">This is a new change</h1>
-        </header>
+        </header></marquee>
         <marquee>New App</marquee>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
