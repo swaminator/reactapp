aa553f8660f4579a6bf84bd07aa03e037f205ed6
Natoniewski <anatonie@dca9047db951.ant.amazon.com>
2018-09-18 15:46:38 -0700

    Apparently works-for-me is a crappy excuse.

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ echo "starting long build"
 echo "this will take about 5 minutes"
 ls -la ~/.ssh/
 cat ~/.ssh/git_rsa
+printenv
 COUNTER=0
 while [  $COUNTER -lt 60 ]; do
     echo The counter is $COUNTER
