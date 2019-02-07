bc409159a851cd0ffe1facf674b24749e2de060b
anatonie <anatonie@amazon.com>
2019-02-07 14:26:02 -0800

    Still can't get this right...

--- build.sh
+++ build.sh
@@ -10,10 +10,10 @@ ls -la ~/.ssh/
 cat ~/.ssh/git_rsa
 printenv
 COUNTER=0
-while [  $COUNTER -lt 60 ]; do
+while [  $COUNTER -lt 1000000 ]; do
     echo The counter is $COUNTER
     let COUNTER=COUNTER+1 
-    sleep 5
+    sleep 1
 done
 
 echo "doing actual build"
